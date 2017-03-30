66cbf8cfdd64938024ae37477c1236f714aaaa66
Lytay <touch.lytay@gaea-sys.com>
2017-03-30 15:52:46 +0700

    Update repository with only apache script file installation

--- apache_setup.sh
+++ apache_setup.sh
@@ -6,10 +6,7 @@
 echo -e "---- Update Server ----"
 sudo apt-get update -y
 sudo apt-get upgrade -y
-
-echo -e "--- Clean unused dependencies ---"
-sudo apt-get autoremove -y
-sudo apt-get clean -y
-sudo apt-get remove -y
-
-echo -e "--- Update completed ---"
+#--------------------------------------
+# Install Apache2
+#--------------------------------------
+sudo apt-get install apache2 -y
