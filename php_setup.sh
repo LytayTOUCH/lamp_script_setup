ad31f1b2e5bcf54f0e39fe6861c9bf7fea8730ad
Lytay <touch.lytay@gaea-sys.com>
2017-04-06 16:37:28 +0700

    Update php setup installation script

--- php_setup.sh
+++ php_setup.sh
@@ -9,4 +9,4 @@ sudo apt-get upgrade -y
 #--------------------------------------
 # Install PHP7.0
 #--------------------------------------
-sudo apt-get install php7.0 php7.0-mysql libapache2-mod-php7.0 php7.0-xml php7.0-zip php7.0-mcrypt -y
+sudo apt-get install php7.0 php7.0-curl php7.0-intl php7.0-xsl php7.0-mysql libapache2-mod-php7.0 php7.0-xml php7.0-zip php7.0-mcrypt -y
